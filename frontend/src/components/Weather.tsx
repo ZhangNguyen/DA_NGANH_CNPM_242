25cd6387872f545835992e559177543a30f8497e
Duyduide <duydante311@gmail.com>
2025-04-21 21:51:49 +0700

    done weather ui

--- frontend/src/components/Weather.tsx
+++ frontend/src/components/Weather.tsx
@@ -1,29 +1,30 @@
-import { Search, Wind, Waves, NavigationOff, CloudFog, Wheat  } from 'lucide-react';
+import { Search, Wind, Waves} from 'lucide-react';
 import clear_png from '@/assets/clear.png'
 import cloud_png from '@/assets/cloud.png'
 import drizzle_png from '@/assets/drizzle.png'
-import humidity_png from '@/assets/humidity.png'
 import rain_png from '@/assets/rain.png'
 import snow_png from '@/assets/snow.png'
-import wind_png from '@/assets/wind.png'
 import { apiGetWeather } from '@/apis/app'
-import { useEffect } from 'react';
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
+import toast from 'react-hot-toast';
 
-// type weatherData = {
-//   humidity: number,
-//   windspeed: number,
-//   tempratute: number,
-// }
-
+interface weatherDataProps {
+  name: string;
+  main: {
+    temp: number;
+    humidity: number;
+  }
+  wind: {
+    speed: number;
+  }
+  weather: {
+    icon: string
+    main: string
+  } []
+}
 const Weather = () => {
-  const [weatherData, setWeatherData] = useState({
-    humidity: 0,
-    windSpeed: 0,
-    temperature: 0,
-    location: null,
-    weatherIcon: clear_png
-  });
+  const inputRef = useRef<HTMLInputElement>(null)
+  const [weatherData, setWeatherData] = useState<weatherDataProps | null>(null);
   const allIcons = {
     '01d': clear_png,
     '01n': clear_png,
@@ -41,43 +42,45 @@ const Weather = () => {
     '13n': snow_png,
   }
   const search = async (cityName: string) => {
+    if (cityName == '') {
+      toast.error('Please enter a city name')
+      return
+    }
     try {
       const response = await apiGetWeather(cityName)
       if (response) {
-        console.log(response.data)
-        const icon = allIcons[response.data.weather[0].icon] || clear_png
-        setWeatherData({
-          humidity: response.data.main.humidity || 0,
-          windSpeed: response.data.wind.speed || 0,
-          temperature: response.data.main.temp || 0,
-          location:  response.data.name || 0,
-          weatherIcon: icon
-        })
+        setWeatherData(response.data)
+        toast.success(`Get ${response.data.name} current weather`)
       }
-      else console.log('API failed')
+      else {
+        toast.error('api wrong')
+      }
     } catch (error) {
-
+      toast.error('City not found!')
     }
   } 
-  useEffect(() => { 
-    //search("Ho Chi Minh")
-  }, [])
+  const icon = allIcons[weatherData?.weather[0]?.icon as keyof typeof allIcons ?? '01d']
+  // useEffect(() => { 
+  //   //search("Ho Chi Minh") 
+  // }, [])
   return (
-    <div className='flex flex-col items-center h-full border rounded-xl place-self-center p-10 border-2 bg-linear-45 from-slate-300 to-gray-400'>
+    <div className='flex flex-col items-center h-full rounded-xl place-self-center p-10 border-2 bg-linear-45 from-slate-300 to-gray-400'>
       {/* search bar */}
       <div className='flex h-10 items-center gap-2'>
-        <input  type="text" 
-                placeholder='Search' 
-                className='outline-0 h-full rounded-2xl pl-6 bg-[#ebfffc]'/>
+        <input
+              ref={inputRef}  
+              type="text" 
+              placeholder='Search' 
+              className='outline-0 h-full rounded-2xl pl-6 bg-[#ebfffc]'/>
         <div className='rounded-full w-full p-2 bg-[#ebfffc] cursor-pointer'>
-          <Search color='gray'/>
+          <Search color='gray' onClick={() => inputRef.current && search(inputRef.current.value)}/>
         </div>
-        
       </div>
-      {/* Location Temp and Weather Icon */}
-      <img src={weatherData.weatherIcon} alt="" className='w-[150px] m-[30px]'/>
-      <p className='text-[60px]/15'>{weatherData.temperature}℃</p>
-      <p className='text-[30px]'>{weatherData.location}</p>
+      {/* Location, Tempratuer, Weather Main, and Weather Icon */}
+      <img src={icon} alt="" className='w-[150px] m-[30px]'/>
+      <p className='text-[60px]/15'>{weatherData?.main.temp}℃</p>
+      <p className='text-[25px]'>{weatherData?.weather[0].main}</p>
+      <p className='text-[30px]'>{weatherData?.name}</p>
       {/* Weather Data */}
       <div className='flex justify-between w-[100%] mt-[40px] m-2'>
         <div className='flex flex-row gap-[12px]'>
@@ -85,7 +88,7 @@ const Weather = () => {
             <Waves size={35}/>
           </div>
           <div>
-            <p>{weatherData.humidity}%</p>
+            <p>{weatherData?.main.humidity}%</p>
             <span>Humidity</span>
           </div>
         </div>
@@ -94,7 +97,7 @@ const Weather = () => {
             <Wind size={35}/>
           </div>
           <div>
-            <p>{weatherData.windSpeed} Km/h</p>
+            <p>{weatherData?.wind.speed} Km/h</p>
             <span>Wind Speed</span>
           </div>
         </div>
